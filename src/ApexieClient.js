d774e348e5f8fc04cdeb62181a7fa0ce79e8834e
abdyy <68809422+AbdYy0@users.noreply.github.com>
2021-07-15 15:57:54 +0200

    added yt notification system

--- src/ApexieClient.js
+++ src/ApexieClient.js
@@ -21,4 +21,31 @@ loadCommands(client);
 client.commands = new Collection();
 client.aliases = new Collection();
 
+client.db = require("quick.db");
+client.request = new (require("rss-parser"))();
+
+//notifiche yt sotto
+
+function handleUploads() {
+    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
+    setInterval(() => {
+        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+        .then(data => {
+            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+            else {
+                client.db.set(`videoData`, data.items[0]);
+                client.db.push("postedVideos", data.items[0].link);
+                let parsed = client.db.fetch(`videoData`);
+                let channel = client.channels.cache.get(client.config.channel);
+                if (!channel) return;
+                let message = client.config.messageTemplate
+                    .replace(/{author}/g, parsed.author)
+                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                    .replace(/{url}/g, parsed.link);
+                channel.send(message);
+            }
+        });
+    }, client.config.watchInterval);
+}
+
 client.login(config.token);

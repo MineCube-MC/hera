93815755f58e0ef0614f74a2a9b59e9c3d2e7651
ItzLightyHD <45460688+ItzLightyHD@users.noreply.github.com>
2022-01-02 21:29:36 +0100

    Ok :YEP:

--- src/Events/messageDelete.ts
+++ src/Events/messageDelete.ts
@@ -5,8 +5,8 @@ import { moderationLogsSchema } from '../Models/moderationLogs';
 export const event: Event = {
     name: 'messageDelete',
     run: async(client, message: Message) => {
-        const modSchema = await moderationLogsSchema.findOne({ guild: message.guild.id }).get('Channel').clone();
-        const logsChannel = client.channels.cache.find(ch => ch.id === modSchema);
+        const modSchema = await moderationLogsSchema.findOne({ guild: message.guild.id }).clone();
+        const logsChannel = client.channels.cache.find(ch => ch.id === modSchema.get('Channel'));
         if(!logsChannel) return;
         if (!((logsChannel): logsChannel is TextChannel => logsChannel.type === 'GUILD_TEXT')(logsChannel)) return;
         let logs = await message.guild.fetchAuditLogs({type: 72});

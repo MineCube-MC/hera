fd9539c521c481a193d3e84be19d67978c499628
ItzLightyHD <45460688+ItzLightyHD@users.noreply.github.com>
2022-03-01 13:25:54 +0100

    Events again

---
+++ src/events/ready.ts
@@ -0,0 +1,9 @@
+import { Event } from "../structures/Event";
+
+export default new Event("ready", (client) => {
+    if(client.isReady) {
+        console.log("Bot is online");
+
+        client.user.setActivity("with Discord", { type: "COMPETING" });
+    }
+});

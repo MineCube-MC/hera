e801b9c6b3b8b2188cd9a72ad5f6f3e835f5122f
ItzLightyHD <45460688+ItzLightyHD@users.noreply.github.com>
2022-03-01 11:45:02 +0100

    Message deleted - Part 2 + Dependencies update

---
+++ src/events/messageDelete.ts
@@ -0,0 +1,57 @@
+import { Role, TextChannel } from "discord.js";
+import guildSchema from "../models/guildSchema";
+import { ExtendedEmbed } from "../structures/Embed";
+import { Event } from "../structures/Event";
+
+export default new Event("messageDelete", async (message) => {
+    const member = message.member;
+
+    let guildData;
+    try {
+        guildData = await guildSchema.findOne({ serverID: member.guild.id });
+        if(!guildData) {
+            let guild = await guildSchema.create({
+                serverID: member.guild.id,
+                welcome: {
+                    enabled: false,
+                    channelID: "none",
+                    text: ":wave: Hello {member}, welcome to {guild}!"
+                },
+                logs: {
+                    enabled: false,
+                    channelID: "none"
+                },
+                autoRoles: [],
+                blacklist: []
+            });
+            guild.save();
+            guildData = await guildSchema.findOne({ serverID: member.guild.id });
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    const logChannel = message.guild.channels.cache.get(guildData.logs.channelID) as TextChannel;
+    let rolesWithPerms: Role[] = [];
+
+    message.guild.roles.cache.forEach((role) => {
+        if(role.permissions.has("MANAGE_MESSAGES") && !role.managed) rolesWithPerms.push(role);
+    });
+
+    let logs = await message.guild.fetchAuditLogs({type: 72});
+    let entry = logs.entries.first();
+
+    if(logChannel) {
+        logChannel.send({
+            embeds: [
+                new ExtendedEmbed()
+                .setTitle("Message deleted")
+                .setDescription(`Someone with the \`MANAGE_MESSAGES\` permission (probably one of these: ${rolesWithPerms.map(role => `${role}`).join(", ")}) deleted the message of a member in this server.`)
+                .setThumbnail(message.member.displayAvatarURL({ dynamic: true }) || message.author.displayAvatarURL({ dynamic: true }))
+                .addField("Author", `${message.member}`, true)
+                .addField("Deleted by", `${entry.executor}`, true)
+                .addField("Content", `\`${message.content}\``)
+            ]
+        });
+    }
+});

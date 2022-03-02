12d88ef8cdb8168da09049f45ad2f15b04a2d980
ItzLightyHD <45460688+ItzLightyHD@users.noreply.github.com>
2022-03-02 08:35:15 +0100

    What a mistake

--- src/models/profileSchema.ts
+++ src/models/profileSchema.ts
@@ -3,7 +3,7 @@ import mongo from "mongoose";
 export default mongo.model(
     "profile",
     new mongo.Schema({
-        userID: { type: String, require: true, unique: true },
+        userID: { type: String, require: true },
         serverID: { type: String, require: true },
         warnings: { type: Number, default: 0 }
     })

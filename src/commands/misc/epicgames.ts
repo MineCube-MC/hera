54c17aa636681d2f5542bd1ddb3ba2196261dcdc
ItzLightyHD <itzlightyhd@apexie.eu>
2022-06-18 19:26:30 +0200

    Free Epic Games

---
+++ src/commands/misc/epicgames.ts
@@ -0,0 +1,36 @@
+import { Command } from "../../structures/Command";
+import { Country, getGames } from "epic-free-games/dist";
+import { ExtendedEmbed } from "../../structures/Embed";
+
+const countries = ["TR", "US", "GB", "DE", "AR", "ES", "MX", "FR", "IT", "JP", "KR", "PL", "BR", "RU", "TH", "CN"];
+const countryChoices = [];
+countries.forEach(country => {
+    countryChoices.push({
+        name: country,
+        value: country
+    });
+});
+
+export default new Command({
+    name: "epicgames",
+    description: "Check for free games on the Epic Games Store",
+    options: [
+        {
+            name: "country",
+            description: "The country you want to check for free games",
+            type: "STRING",
+            required: true,
+            choices: countryChoices
+        }
+    ],
+    run: async ({ interaction, args }) => {
+        const country = args.getString("country");
+        const games = await getGames(country as Country, true);
+        const gameEmbed = new ExtendedEmbed()
+            .setTitle(games.currentGames[0].title)
+            .setDescription(games.currentGames[0].description)
+            .setThumbnail(games.currentGames[0].keyImages[0].url);
+        if(games.currentGames[0].url) gameEmbed.setURL(games.currentGames[0].urlSlug);
+        interaction.reply({ embeds: [gameEmbed] });
+    }
+});

a7502acdf36b4a236292acb13cee2698736e4f23
ItzLightyHD <itzlightyhd@apexie.eu>
2022-07-03 10:10:29 +0200

    Ok, update is ready?

---
+++ scripts/socketUpdate.js
@@ -0,0 +1,19 @@
+const socket = new WebSocket("ws://api.plenusbot.xyz:5944");
+const key = process.env.socketKey;
+
+socket.onopen = () => {
+    console.log("Socket connected");
+    socket.send(JSON.stringify({
+        type: "update",
+        key: key
+    }));
+}
+
+socket.onmessage = (message) => {
+    const data = JSON.parse(message.data);
+    if (data.message === "RESTARTING") {
+        console.log("Successfully restarted the bot with the updated version");
+        socket.close();
+        process.exit(0);
+    }
+}

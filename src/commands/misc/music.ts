82648d25cfceb4e82163301cb3b8f85567ef5bda
Anthony <imlighty@apexie.dev>
2023-07-14 13:15:59 +0000

    Music actually working with bun

--- src/commands/misc/music.ts
+++ src/commands/misc/music.ts
@@ -1,6 +1,8 @@
-import { ApplicationCommandOptionType } from "discord.js";
-import { Command } from "../../structures/Command";
-import { MusicEmbed } from "../../structures/Embed";
+import { ApplicationCommandOptionType } from "discord.js"
+import { Command } from "../../structures/Command"
+import { MusicEmbed } from "../../structures/Embed"
+import { client } from '../..'
+import { MoonlinkQueue } from 'moonlink.js'
 
 // Credit to Kaj on YouTube for the music system code
 
@@ -22,19 +24,19 @@ export default new Command({
       ],
     },
     {
-      name: "volume",
-      description: "Change the volume of the music player",
+      name: "queue",
+      description: "See what's in the current queue",
       type: ApplicationCommandOptionType.Subcommand,
-      options: [
-        {
-          name: "percent",
-          description: "The volume percentage without the % (1-100)",
-          type: ApplicationCommandOptionType.Integer,
-          required: true,
-          min_value: 1,
-          max_value: 100,
-        },
-      ],
+    },
+    {
+      name: "skip",
+      description: "Skip the current song",
+      type: ApplicationCommandOptionType.Subcommand,
+    },
+    {
+      name: "stop",
+      description: "Stop the queue and disconnect the bot from your voice channel",
+      type: ApplicationCommandOptionType.Subcommand,
     },
     {
       name: "options",
@@ -48,34 +50,31 @@ export default new Command({
           required: true,
           choices: [
             { name: "loop", value: "loop" },
-            { name: "queue", value: "queue" },
-            { name: "skip", value: "skip" },
             { name: "pause", value: "pause" },
             { name: "resume", value: "resume" },
-            { name: "stop", value: "stop" },
           ],
         },
       ],
     },
   ],
   run: async ({ interaction, args, client }) => {
-    const { member, guild, channel } = interaction;
+    const { member, guild, channel } = interaction
 
-    const subcommand = args.getSubcommand();
-    const query = args.getString("query");
-    const volume = args.getInteger("percent");
-    const option = args.getString("option");
-    const voiceChannel = member.voice.channel;
+    const subcommand = args.getSubcommand()
+    const query = args.getString("query")
+    const volume = args.getInteger("percent")
+    const option = args.getString("option")
+    const voiceChannel = member.voice.channel
 
-    const embed = new MusicEmbed();
+    const embed = new MusicEmbed()
 
     if (!voiceChannel) {
       embed
         .setColor("Red")
         .setDescription(
-          "You need to be in a voice channel to use this command"
-        );
-      return interaction.reply({ embeds: [embed], ephemeral: true });
+          ":warning: | You need to be in a voice channel to use this command"
+        )
+      return interaction.reply({ embeds: [embed], ephemeral: true })
     }
 
     if (guild.members.me.voice.channel) {
@@ -83,91 +82,203 @@ export default new Command({
         embed
           .setColor("Red")
           .setDescription(
-            "You need to be in the same voice channel as me to use this command"
-          );
+            ":warning: | You need to be in the same voice channel as me to use this command"
+          )
         return interaction.reply({
           embeds: [embed],
           ephemeral: true,
-        });
+        })
       }
     }
+
+    let player = client.moon.players.create({
+      guildId: interaction.guild.id,
+      voiceChannel: interaction.member.voice.channel.id,
+      textChannel: interaction.channel.id,
+      autoPlay: true
+    })
 
     try {
       switch (subcommand) {
         case "play":
-          client.distube.play(voiceChannel, query, {
-            textChannel: channel,
-            member: member,
-          });
-          return interaction.reply({
-            content: `🎶 Searching \`${query}\`...`,
-          });
-        case "volume":
-          client.distube.setVolume(voiceChannel, volume);
-          return interaction.reply({
-            content: `🔊 Volume set to ${volume}%`,
-          });
+          if (!player.connected) player.connect({
+            setDeaf: true,
+            setMute: false
+          })
+          let res = await client.moon.search(query)
+          if (res.loadType === 'error') {
+            return interaction.reply({
+              content: `⛔ | Load failed.`
+            })
+          } else if (res.loadType === "empty") {
+            return interaction.reply({
+              content: `⛔ | No matches!`
+            }) // nothing was found
+          }
+          if (res.loadType === 'playlist') {
+            embed
+              .setColor("Green")
+              .setDescription(`🎶 | Added \`${res.playlistInfo.name}\`\nRequested by: <@${interaction.user.id}>`)
+            interaction.reply({
+              embeds: [embed]
+            })
+            for (const track of res.tracks) {
+              player.queue.add(track)
+            }
+          } else {
+            let formattedDuration: string
+            let seconds = roundToInt(res.tracks[0].duration / 1000)
+            let minutes = roundToInt(seconds / 60)
+            if (seconds / 60 > 0) {
+              formattedDuration = `${separateDigits(minutes).length === 1 ? "0" : ""}${minutes}:${separateDigits(seconds).length === 1 ? "0" : ""}${seconds}`
+            } else {
+              formattedDuration = seconds + seconds > 1 ? " seconds" : " second"
+            }
+            player.queue.add(res.tracks[0])
+            embed.setColor("Green")
+              .setDescription(`🎶 | Added \`${res.tracks[0].title}\` - \`${formattedDuration
+                }\`\nRequested by: <@${interaction.user.id}>`)
+              .setThumbnail(res.tracks[0].artworkUrl)
+            interaction.reply({
+              embeds: [embed]
+            })
+          }
+          if (!player.playing) player.play()
+          break
+        case "stop":
+          if (player.connected) {
+            embed
+              .setColor("Blue")
+              .setDescription(":firecracker: | Player destroyed!")
+            player.destroy().then(status => {
+              if (status)
+                interaction.reply({
+                  embeds: [embed]
+                })
+              else {
+                embed
+                  .setColor("Red")
+                  .setDescription("⛔ | There's an opposing force that doesn't let the player to be destroyed, can you find it?")
+                interaction.reply({ embeds: [embed] })
+              }
+            })
+          } else {
+            embed
+              .setColor("Red")
+              .setDescription("⛔ | There is no active queue.")
+            interaction.reply({
+              embeds: [embed],
+              ephemeral: true
+            })
+          }
+          break
+        case "skip":
+          if (!player.connected) {
+            embed
+              .setColor("Red")
+              .setDescription("⛔ | There is no active queue.")
+            return interaction.reply({
+              embeds: [embed],
+              ephemeral: true
+            })
+          }
+          player.skip()
+          let formattedDuration: string
+          let seconds = roundToInt(player.current.duration / 1000)
+          let minutes = roundToInt(seconds / 60)
+          if (seconds / 60 > 0) {
+            formattedDuration = `${separateDigits(minutes).length === 1 ? "0" : ""}${minutes}:${separateDigits(seconds).length === 1 ? "0" : ""}${seconds}`
+          } else {
+            formattedDuration = seconds + seconds > 1 ? " seconds" : " second"
+          }
+          embed.setColor("Blue")
+            .setDescription(`🎶 | Now playing \`${player.current.title}\` - \`${formattedDuration
+              }\`\nSkipped by: <@${interaction.user.id}>`)
+            .setThumbnail(player.current.artworkUrl)
+          interaction.reply({
+            embeds: [embed]
+          })
+          break
         case "options":
-          const queue = await client.distube.getQueue(voiceChannel);
-
-          if (!queue) {
-            embed.setColor("Red").setDescription("There is no active queue.");
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+          if (!player.connected) {
+            embed.setColor("Red").setDescription("⛔ | There is no active queue.")
+            return interaction.reply({ embeds: [embed], ephemeral: true })
           }
 
           switch (option) {
             case "loop":
-              queue.setRepeatMode();
+              if (player.loop == 0)
+                player.setLoop(1)
+              else
+                player.setLoop(0)
               embed
                 .setColor("Purple")
                 .setDescription(
-                  `🔁 Looping has been ${
-                    queue.repeatMode == 1 ? "enabled" : "disabled"
+                  `🔁 Looping has been ${player.loop == 1 ? "enabled" : "disabled"
                   }`
-                );
-              return interaction.reply({ embeds: [embed], ephemeral: true });
-            case "skip":
-              await queue.skip();
-              embed
-                .setColor("Blue")
-                .setDescription("⏩ The song has been skipped.");
-              return interaction.reply({ embeds: [embed], ephemeral: true });
-            case "stop":
-              await queue.stop();
-              embed
-                .setColor("Red")
-                .setDescription("⏹️ The song has been stopped.");
-              return interaction.reply({ embeds: [embed], ephemeral: true });
+                )
+              return interaction.reply({ embeds: [embed], ephemeral: true })
             case "pause":
-              queue.pause();
+              if (player.paused) return interaction.reply({
+                embeds: [
+                  embed
+                    .setColor("Red")
+                    .setDescription("⛔ | The player is already paused")
+                ],
+                ephemeral: true
+              })
+              player.pause()
               embed
                 .setColor("Orange")
-                .setDescription("⏸️ The song has been paused.");
-              return interaction.reply({ embeds: [embed], ephemeral: true });
+                .setDescription("⏸️ The player has been paused.")
+              return interaction.reply({ embeds: [embed], ephemeral: true })
             case "resume":
-              queue.resume();
+              if (!player.paused) return interaction.reply({
+                embeds: [
+                  embed
+                    .setColor("Red")
+                    .setDescription("⛔ | The player is already resumed")
+                ],
+                ephemeral: true
+              })
+              player.resume()
               embed
                 .setColor("Green")
-                .setDescription("▶️ The song has been resumed.");
-              return interaction.reply({ embeds: [embed], ephemeral: true });
-            case "queue":
-              embed
-                .setColor("Purple")
-                .setDescription(
-                  `${queue.songs.map(
-                    (song, id) =>
-                      `\n**${id + 1}**. ${song.name} - \`${
-                        song.formattedDuration
-                      }\``
-                  )}`
-                );
-              return interaction.reply({ embeds: [embed], ephemeral: true });
+                .setDescription("▶️ The song has been resumed.")
+              return interaction.reply({ embeds: [embed], ephemeral: true })
           }
+          break
       }
     } catch (e) {
-      console.error(e);
-      embed.setColor("Red").setDescription("`⛔ | An error occurred...`");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
+      console.error(e)
+      embed.setColor("Red").setDescription("`⛔ | An error occurred...`")
+      return interaction.reply({ embeds: [embed], ephemeral: true })
     }
   },
-});
+})
+
+type Int = number & { __int__: void }
+
+const roundToInt = (num: number): Int => Math.round(num) as Int
+
+function separateDigits(num: number): number[] {
+  let arr: number[] = []
+  let lastDigit: number
+
+  num = toInt32(num)
+
+  do {
+    lastDigit = num % 10
+    arr.push(lastDigit)
+    // Updating num to num/10 cuts off the last digit:
+    num = toInt32(num / 10)
+  }
+  while (num !== 0)
+
+  return arr.reverse()
+}
+
+function toInt32(f: number): number {
+  // Note that type "number" in JS is always "float" internally.
+  return f >> 0
+}
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -5,182 +5,53 @@ import {
   ClientEvents,
   Collection,
   VoiceChannel,
-} from "discord.js";
-import { CommandType } from "../typings/Command";
-import { RegisterCommandsOptions } from "../typings/client";
-import { Event } from "./Event";
-import { GiveawaysManager } from "discord-giveaways";
-import { MusicEmbed } from "./Embed";
-import { DisTube } from "distube";
-import { SpotifyPlugin } from "@distube/spotify";
-import { SoundCloudPlugin } from "@distube/soundcloud";
-import globPromise from "glob-promise";
+} from "discord.js"
+import { CommandType } from "../typings/Command"
+import { RegisterCommandsOptions } from "../typings/client"
+import { Event } from "./Event"
+import { GiveawaysManager } from "discord-giveaways"
+import { MusicEmbed } from "./Embed"
+import globPromise from "glob-promise"
+import { MoonlinkManager } from 'moonlink.js'
 
 export class ExtendedClient extends Client {
-  commands: Collection<string, CommandType> = new Collection();
-  privateCommands: Collection<string, CommandType> = new Collection();
-  sweepMessages = this.sweepers.sweepMessages;
-  giveaways: GiveawaysManager;
-  distube: DisTube;
+  commands: Collection<string, CommandType> = new Collection()
+  privateCommands: Collection<string, CommandType> = new Collection()
+  sweepMessages = this.sweepers.sweepMessages
+  giveaways: GiveawaysManager
+  moon: MoonlinkManager
 
-  constructor() {
-    super({ intents: 98303 });
-  }
+  constructor() { super({ intents: 98303 }) }
 
   async start() {
-    this.registerModules();
-    this.login(process.env.botToken);
+    this.registerModules()
+    this.login(process.env.botToken)
 
-    this.on("ready", () => {});
+    this.on("ready", () => {
+      this.moon.init(this.user.id)
+    })
 
-    this.distube = new DisTube(this, {
-      leaveOnStop: true,
-      emitNewSongOnly: true,
-      emitAddSongWhenCreatingQueue: false,
-      emitAddListWhenCreatingQueue: false,
-      plugins: [
-        new SpotifyPlugin({
-          emitEventsAfterFetching: true,
-        }),
-        new SoundCloudPlugin(),
-      ],
-    });
+    this.on('raw', (data) => {
+      this.moon.packetUpdate(data)
+    })
 
-    // On bot disconnect, stop the music
-    this.on("voiceStateUpdate", (oldState, newState) => {
-      if (
-        oldState.member?.id === this.user?.id &&
-        oldState.channel?.members.size === 1
-      ) {
-        const queue = this.distube.getQueue(oldState.guild.id);
-        if (queue) queue.stop();
+    this.moon = new MoonlinkManager(
+      [{ host: "localhost", port: 2333, password: "youshallnotpass" }],
+      {
+
+      },
+      (guild, sPayload) => {
+        this.guilds.cache.get(guild).shard.send(JSON.parse(sPayload))
       }
-    });
+    )
 
-    const status = (queue) =>
-      `Volume: \`${queue.volume}%\` | Filter: \`${
-        queue.filters.names.join(", ") || "Off"
-      }\` | Loop: \`${
-        queue.repeatMode
-          ? queue.repeatMode === 2
-            ? "All Queue"
-            : "This Song"
-          : "Off"
-      }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
-    this.distube
-      .on("disconnect", (queue) => {
-        queue.stop();
-      })
-      .on("playSong", (queue, song) => {
-        if (
-          queue.textChannel instanceof BaseGuildTextChannel ||
-          queue.textChannel instanceof VoiceChannel
-        ) {
-          queue.textChannel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Green")
-                .setDescription(
-                  `🎶 | Playing \`${song.name}\` - \`${
-                    song.formattedDuration
-                  }\`\nRequested by: ${song.user}\n${status(queue)}`
-                )
-                .setThumbnail(song.thumbnail),
-            ],
-          });
-        }
-      })
-      .on("addSong", (queue, song) => {
-        if (
-          queue.textChannel instanceof BaseGuildTextChannel ||
-          queue.textChannel instanceof VoiceChannel
-        ) {
-          queue.textChannel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Green")
-                .setDescription(
-                  `🎶 | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-                )
-                .setThumbnail(song.thumbnail),
-            ],
-          });
-        }
-      })
-      .on("addList", (queue, playlist) => {
-        if (
-          queue.textChannel instanceof BaseGuildTextChannel ||
-          queue.textChannel instanceof VoiceChannel
-        )
-          queue.textChannel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Green")
-                .setDescription(
-                  `🎶 | Added \`${playlist.name}\` playlist (${
-                    playlist.songs.length
-                  } songs) to queue\n${status(queue)}`
-                )
-                .setThumbnail(playlist.thumbnail),
-            ],
-          });
-      })
-      .on("error", (channel, e) => {
-        if (
-          channel &&
-          (channel instanceof BaseGuildTextChannel ||
-            channel instanceof VoiceChannel)
-        )
-          channel.send(
-            `⛔ | An error encountered: ${e.toString().slice(0, 1974)}`
-          );
-        else console.error(e);
-      })
-      .on("empty", (queue) => {
-        if (
-          queue.textChannel instanceof BaseGuildTextChannel ||
-          queue.textChannel instanceof VoiceChannel
-        )
-          queue.textChannel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Red")
-                .setDescription(
-                  "⛔ | Voice channel is empty! Leaving the channel..."
-                ),
-            ],
-          });
-      })
-      .on("searchNoResult", (message, query) => {
-        if (
-          message.channel instanceof BaseGuildTextChannel ||
-          message.channel instanceof VoiceChannel
-        )
-          message.channel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Red")
-                .setDescription(`⛔ | No result found for \`${query}\`!`),
-            ],
-          });
-      })
-      .on("finish", (queue) => {
-        if (
-          queue.textChannel instanceof BaseGuildTextChannel ||
-          queue.textChannel instanceof VoiceChannel
-        )
-          queue.textChannel.send({
-            embeds: [
-              new MusicEmbed()
-                .setColor("Green")
-                .setDescription("🏁 | Queue finished!"),
-            ],
-          });
-      });
+    this.moon.on('nodeCreate', () => {
+      console.log(`Lavalink node was connected`)
+    })
   }
 
   async importFile(filePath: string) {
-    return (await import(filePath))?.default;
+    return (await import(filePath))?.default
   }
 
   async registerCommands({ commands, guildId }: RegisterCommandsOptions) {
@@ -189,61 +60,61 @@ export class ExtendedClient extends Client {
         process.env.environment === "dev" ||
         process.env.environment === "debug"
       ) {
-        this.guilds.cache.get(guildId)?.commands.set(commands);
+        this.guilds.cache.get(guildId)?.commands.set(commands)
         console.log(
           `Registering commands to ${this.guilds.cache.get(guildId).name}`
-        );
+        )
       } else {
-        this.application?.commands.set(commands);
-        console.log("Registering global commands");
+        this.application?.commands.set(commands)
+        console.log("Registering global commands")
       }
     } else {
-      this.application?.commands.set(commands);
-      console.log("Registering global commands");
+      this.application?.commands.set(commands)
+      console.log("Registering global commands")
     }
   }
 
   async registerModules() {
     // Commands
-    const slashCommands: ApplicationCommandDataResolvable[] = [];
+    const slashCommands: ApplicationCommandDataResolvable[] = []
     const commandFiles = await globPromise(
       `${__dirname}/../commands/*/*{.ts,.js}`
-    );
+    )
     commandFiles.forEach(async (filePath) => {
-      const command: CommandType = await this.importFile(filePath);
-      if (!command.name) return;
-      if (process.env.environment === "debug") console.log(command);
+      const command: CommandType = await this.importFile(filePath)
+      if (!command.name) return
+      if (process.env.environment === "debug") console.log(command)
 
-      this.commands.set(command.name, command);
-      slashCommands.push(command);
-    });
+      this.commands.set(command.name, command)
+      slashCommands.push(command)
+    })
 
-    const privateSlashCommands: ApplicationCommandDataResolvable[] = [];
+    const privateSlashCommands: ApplicationCommandDataResolvable[] = []
     const privateCommandFiles = await globPromise(
       `${__dirname}/../modules/*/*{.ts,.js}`
-    );
+    )
     privateCommandFiles.forEach(async (filePath) => {
-      const command: CommandType = await this.importFile(filePath);
-      if (!command.name) return;
-      if (process.env.environment === "debug") console.log(command);
+      const command: CommandType = await this.importFile(filePath)
+      if (!command.name) return
+      if (process.env.environment === "debug") console.log(command)
 
-      this.privateCommands.set(command.name, command);
-      privateSlashCommands.push(command);
-    });
+      this.privateCommands.set(command.name, command)
+      privateSlashCommands.push(command)
+    })
 
     this.on("ready", () => {
       this.registerCommands({
         commands: slashCommands,
         guildId: process.env.guildId,
-      });
-    });
+      })
+    })
 
     // Event
-    const eventFiles = await globPromise(`${__dirname}/../events/*{.ts,.js}`);
+    const eventFiles = await globPromise(`${__dirname}/../events/*{.ts,.js}`)
     eventFiles.forEach(async (filePath) => {
-      const event: Event<keyof ClientEvents> = await this.importFile(filePath);
-      this.on(event.event, event.run);
-    });
+      const event: Event<keyof ClientEvents> = await this.importFile(filePath)
+      this.on(event.event, event.run)
+    })
 
     // Giveaways
     this.giveaways = new GiveawaysManager(this, {
@@ -254,6 +125,6 @@ export class ExtendedClient extends Client {
         embedColorEnd: "DarkRed",
         reaction: "🎉",
       },
-    });
+    })
   }
 }
